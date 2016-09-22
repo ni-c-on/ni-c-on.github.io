e7a3fa8bc666e46666fafd9640f1db447e175ef1
nicon <nicon.work@gmail.com>
2016-09-22 20:16:59 +0400

    Update menu, add 404 page

--- js/app.js
+++ js/app.js
@@ -3,9 +3,34 @@ var app = angular.module('app', ['ngRoute']);
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
+      title: 'resume.php',
+      lang: 'php',
       templateUrl: '/views/index.html'
     })
     .when('/photo', {
+      title: 'photo.txt',
       templateUrl: '/views/photo.html'
     })
+    .when('/404', {
+      title: '404.html',
+      lang: 'html',
+      templateUrl: '/views/404.html'
+    })
+    .otherwise({
+      redirectTo: '/404'
+    })
+});
+
+app.controller('menuCtrl', function ($scope, $location, $route) {
+  $scope.isActive = function (url) {
+    return url == $location.url();
+  }
+});
+
+app.run(function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.title;
+        $rootScope.lang = current.lang;
+
+    });
 });
